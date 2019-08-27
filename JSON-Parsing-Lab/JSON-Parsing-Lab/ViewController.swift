bf5f06b0bc87c03b69d059a04fdb6f78c4bfeb7c
aglegaspi <lex83nyc@gmail.com>
2019-08-27 14:35:16 -0400

    creating folder for Supporting Files in Workspace

---
+++ JSON-Parsing-Lab/JSON-Parsing-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON-Parsing-Lab
+//
+//  Created by Alexander George Legaspi on 8/27/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
